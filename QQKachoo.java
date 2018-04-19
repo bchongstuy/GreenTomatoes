1d48bfaba41482f36d3f7c9fecf0b63571c28b61
Brian Lin <blin5@stuy.edu>
2018-04-19 05:16:05 -0400

    Commentify and finish some methods

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,16 +1,30 @@
+/*****************************************************
+ * class QQKachoo
+ * Implements DLLNode, for use in deques
+ *****************************************************/
 
 public class QQKachoo<D> implements Deque<D> {
+    private int _size;
+    private DLLNode<D> _front;
+    private DLLNode<D> _end;
+
+    public QQKachoo() {
+	_size = 0;
+	_front = _end = null;
+    }
     
     public void addLast() {
     }
-    
+    /*
     public Card pollFirst() {
     }
     
     public Card peekFirst() {
+         return _front.getValue();
     }
     
     public int size() {
+         return _size;
     }
     
     public boolean offerLast() {
@@ -23,11 +37,12 @@ public class QQKachoo<D> implements Deque<D> {
     }
     
     public Card peekLast() {
+         return _end.getValue();
     }
     
     public boolean offerFirst() {
     }
-    
+    */
     public static void main(String[] args) {
     }
     
