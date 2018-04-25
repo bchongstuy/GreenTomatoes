02e5f1aff89b7836e9e9a5cc3e8f20201e1e9ece
Brian Lin <16246866+BianoBonzoIII@users.noreply.github.com>
2018-04-25 17:36:52 -0400

    Update Deque.java

--- Deque.java
+++ Deque.java
@@ -12,17 +12,11 @@ public interface Deque<Card> {
     public Card peekFirst();
 
     public int size();
-
-
-
+    
     public void addFirst(Card insert);  
   
     public Card pollLast();
     
     public Card peekLast();
-    
-
-    /*
-    */
     
 }//end interface Deque
